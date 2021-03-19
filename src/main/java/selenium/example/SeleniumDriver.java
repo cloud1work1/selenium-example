517a9ae717cbe7a0beef0f4852340465d0894b17
unknown <cloud1work1@gmail.com>
2021-03-19 15:32:06 +0530

    Initial Commit

---
+++ src/main/java/selenium/example/SeleniumDriver.java
@@ -0,0 +1,23 @@
+package selenium.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SeleniumDriver {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.amazon.in/");
+		WebElement element = driver.findElement(By.id("twotabsearchtextbox"));
+		element.sendKeys("Iphone");
+		element.sendKeys(Keys.ENTER);
+		driver.quit();
+	}
+}
